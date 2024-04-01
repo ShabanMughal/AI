180489f2e5c0de9c85be77052e337804c4cfb546
ShabanMughal <user.thandermughal@gmail.com>
2024-04-01 12:10:32 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Hero from './components/Hero'
+import Benefits from './components/Benefits'
+import Collaboration from './components/Collaboration'
+import Services from './components/Services'
+import Pricing from './components/Pricing'
+import Roadmap from './components/Roadmap'
+import Footer from './components/Footer'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='pt-[4.75rem] lg:pt-[5.23rem] overflow-hidden'>
+     <Header />
+     <Hero />
+     <Benefits />
+     <Collaboration />
+     <Services />
+     <Pricing />
+     <Roadmap />
+     <Footer />
+     </div>
+     <ButtonGradient />
+    </>
+  )
+}
+
+export default App
